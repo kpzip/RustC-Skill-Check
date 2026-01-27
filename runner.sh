911aa90e6282c0f699384e3e62e6c1a5aac8e322
Kyle Schmerge <kyleschmerge1@gmail.com>
2026-01-27 10:49:17 -0800

    w

---
+++ runner.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd rustc-skill-check/src/minecraft
+./gradlew runClient
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rustc_skill_check::skill_check;
+
+fn main() {
+    skill_check!();
+    println!("Hello, world!");
+}

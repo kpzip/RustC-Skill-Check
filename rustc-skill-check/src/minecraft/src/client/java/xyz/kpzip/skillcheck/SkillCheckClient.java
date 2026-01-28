d9b14793cd4b4b9d224a00255362bd13a8225944
Kyle Schmerge <kyleschmerge1@gmail.com>
2026-01-28 00:31:38 -0800

    rename

--- rustc-skill-check/src/minecraft/src/client/java/xyz/kpzip/skillcheck/SkillCheckClient.java
+++ rustc-skill-check/src/minecraft/src/client/java/xyz/kpzip/skillcheck/SkillCheckClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package xyz.kpzip.skillcheck;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class SkillCheckClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.

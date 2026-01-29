3222ed2f04e18268b01bb06c697f203eba365215
Kyle Schmerge <kyleschmerge1@gmail.com>
2026-01-28 23:39:46 -0800

    inject

--- rustc-skill-check/src/minecraft/src/client/java/xyz/kpzip/skillcheck/mixin/client/MainMixin.java
+++ rustc-skill-check/src/minecraft/src/client/java/xyz/kpzip/skillcheck/mixin/client/MainMixin.java
@@ -1,15 +1,18 @@
 package xyz.kpzip.skillcheck.mixin.client;
 
-import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
-public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of Minecraft.run()V
+import net.minecraft.client.main.Main;
+
+@Mixin(Main.class)
+public class MainMixin {
+	
+	@Inject(at = @At("TAIL"), method = "main")
+	private static void main(CallbackInfo info) {
+		System.exit(-1);
 	}
+	
 }
